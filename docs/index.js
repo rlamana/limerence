32b313c66e698f0dc5f6e2d0fb7ecebc5507d044
Ramón Lamana <rlamana@gmail.com>
2015-08-21 00:04:29 +0200

    Fixes docs typo

--- docs/index.js
+++ docs/index.js
@@ -5,10 +5,5 @@ document.addEventListener("DOMContentLoaded", function() {
     // Documentation details
     var details = document.querySelectorAll('.details');
     Array.prototype.forEach.call(details, function(element) {
-        element.style.display = 'none';
-
-        element.addEventListener('click', function() {
-            console.log('hello');
-        })
     });
 });
